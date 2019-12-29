2d86396eaa85811c2e25b16c3aa62d3257f90695
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2019-12-29 09:21:28 -0500

    updated pages, background, content, and questionairre

---
+++ app/data/starting.js
@@ -0,0 +1,103 @@
+let profiles = [
+    {
+      name: "Mihcelle Obama",
+      photo: ,
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Jake Paul",
+      photo: ,
+      scores: [
+        "5",
+        "3",
+        "2",
+        "0",
+        "4",
+        "3",
+        "1",
+        "2",
+        "3",
+        "5"
+      ]
+    },
+    {
+      name: "George Clooney",
+      photo: ,
+      scores: [
+        "4",
+        "3",
+        "1",
+        "3",
+        "5",
+        "3",
+        "2",
+        "1",
+        "4",
+        "4"
+      ]
+    },
+    {
+      name: "Emma Watson",
+      photo: ,
+      scores: [
+        "4",
+        "5",
+        "2",
+        "3",
+        "1",
+        "2",
+        "4",
+        "3",
+        "4",
+        "4"
+      ]
+    },
+    {
+      name: "Matt Damon",
+      photo: ,
+      scores: [
+        "3",
+        "4",
+        "5",
+        "2",
+        "4",
+        "3",
+        "4",
+        "2",
+        "3",
+        "4"
+      ]
+    },
+    {
+      name: "Brittney Spears",
+      photo: ,
+      scores: [
+        "3",
+        "2",
+        "1",
+        "4",
+        "3",
+        "1",
+        "4",
+        "3",
+        "5",
+        "4"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = profiles;
+  
---
+++ app/routing/api.js
@@ -0,0 +1,54 @@
+// Link data to source file
+let profiles = require("../data/profiles");
+
+module.exports = function(app) {
+  // Feeds back json data when app/starter page is visited
+  app.get("/api/profiles", function(req, res) {
+    res.json(profiles);
+  });
+
+  // Action when use submits survey
+  app.post("/api/starters", function(req, res) {
+    let profileMatch = {
+      name: "",
+      photo: "",
+      prolfileDifference: 100
+    };
+
+    // Store results from survery to parse
+    let userData = req.body;
+    let userScores = userData.scores;
+
+    // Holds difference between database and user score
+    let totalDifference;
+
+    // Iterate through all starter sin database.
+    for (let i = 0; i < profiles.length; i++) {
+      let checkProfiles = profiles[i];
+      totalDifference = 0;
+
+      // Loop through all scores
+      for (let j = 0; j < checkProfiles.scores.length; j++) {
+        let currentprofilescore = checkProfiles.scores[j];
+        let currentUserScore = userScores[j];
+
+        // Calulate differences
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentprofilescore));
+      }
+
+      // If the sum of differences is less then the differences of the current "best match"
+      if (totalDifference <= profileMatch.starterDifference) {
+        // Reset the starterMatch to be the new starter.
+        prolfileMatch.name = checkProfiles.name;
+        prolfileMatch.photo = checkProfiles.photo;
+        profileMatch.profileDifference = totalDifference;
+      }
+    }
+
+    // Save user data as new pokemon
+    profiles.push(userData);
+
+    // Return a JSON with the user's starterMatch. This will be used by the HTML in the next page
+    res.json(profileMatch);
+  });
+};
